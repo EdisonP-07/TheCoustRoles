e0eebcf91035e40f2f23b1fd9d3ef5067c145728
EdisonP-07 <edde091@gmail.com>
2025-11-14 17:08:40 +0100

    Create HatsLoader.cs

---
+++ TheCoustRoles/Modules/CustomHats/HatsLoader.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.IO;
+using System.Text.Json;
+using BepInEx.Unity.IL2CPP.Utils;
+using TheCoustRoles.Utilities;
+using UnityEngine;
+using UnityEngine.Networking;
+using static TheCoustRoles.Modules.CustomHats.CustomHatManager;
+
+namespace TheCoustRoles.Modules.CustomHats;
+
+public class HatsLoader : MonoBehaviour
+{
+    private bool isRunning;
+
+    public void FetchHats()
+    {
+        if (isRunning) return;
+        this.StartCoroutine(CoFetchHats());
+    }
+
+    [HideFromIl2Cpp]
+    private IEnumerator CoFetchHats()
+    {
+        isRunning = true;
+        var www = new UnityWebRequest();
+        www.SetMethod(UnityWebRequest.UnityWebRequestMethod.Get);
+        TheCoustRolesPlugin.Logger.LogMessage($"Download manifest at: {RepositoryUrl}/{ManifestFileName}");
+        www.SetUrl($"{RepositoryUrl}/{ManifestFileName}");
+        www.downloadHandler = new DownloadHandlerBuffer();
+        var operation = www.SendWebRequest();
+
+        while (!operation.isDone)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            TheCoustRolesPlugin.Logger.LogError(www.error);
+            yield break;
+        }
+
+        var response = JsonSerializer.Deserialize<SkinsConfigFile>(www.downloadHandler.text, new JsonSerializerOptions
+        {
+            AllowTrailingCommas = true
+        });
+        www.downloadHandler.Dispose();
+        www.Dispose();
+
+        if (!Directory.Exists(HatsDirectory)) Directory.CreateDirectory(HatsDirectory);
+
+        UnregisteredHats.AddRange(SanitizeHats(response));
+        var toDownload = GenerateDownloadList(UnregisteredHats);
+        if (EventUtility.isEnabled) UnregisteredHats.AddRange(CustomHatManager.loadHorseHats());
+
+        TheCoustRolesPlugin.Logger.LogMessage($"I'll download {toDownload.Count} hat files");
+
+        foreach (var fileName in toDownload)
+        {
+            yield return CoDownloadHatAsset(fileName);
+        }
+
+        isRunning = false;
+    }
+
+    private static IEnumerator CoDownloadHatAsset(string fileName)
+    {
+        var www = new UnityWebRequest();
+        www.SetMethod(UnityWebRequest.UnityWebRequestMethod.Get);
+        fileName = fileName.Replace(" ", "%20");
+        TheCoustRolesPlugin.Logger.LogMessage($"downloading hat: {fileName}");
+        www.SetUrl($"{RepositoryUrl}/hats/{fileName}");
+        www.downloadHandler = new DownloadHandlerBuffer();
+        var operation = www.SendWebRequest();
+
+        while (!operation.isDone)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            TheCoustRolesPlugin.Logger.LogError(www.error);
+            yield break;
+        }
+
+        var filePath = Path.Combine(HatsDirectory, fileName);
+        filePath = filePath.Replace("%20", " ");
+        var persistTask = File.WriteAllBytesAsync(filePath, www.downloadHandler.data);
+        while (!persistTask.IsCompleted)
+        {
+            if (persistTask.Exception != null)
+            {
+                TheCoustRolesPlugin.Logger.LogError(persistTask.Exception.Message);
+                break;
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        www.downloadHandler.Dispose();
+        www.Dispose();
+    }
+}
