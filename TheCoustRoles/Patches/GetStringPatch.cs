974fdc11d4c9d6b6b8d8655b937dfd4d0111006a
EdisonP-07 <edde091@gmail.com>
2025-11-14 20:52:13 +0100

    Update GetStringPatch.cs

--- TheCoustRoles/Patches/GetStringPatch.cs
+++ TheCoustRoles/Patches/GetStringPatch.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TheOtherRoles.Patches {
+namespace TheCoustRoles.Patches {
     [HarmonyPatch]
     class GetStringPatch {
         [HarmonyPatch(typeof(TranslationController), nameof(TranslationController.GetString), new[] {
